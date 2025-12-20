62770e25f7d1ecd76e93cb7f5c10e58340bdd93c
Zach <zag0003@auburn.edu>
2025-12-20 06:34:12 -0600

    added swear testing event

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 use dotenv::dotenv;
-use poise::serenity_prelude as serenity;
+use poise::serenity_prelude::{self as serenity, FullEvent};
 
 struct Data {}
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
+
+async fn event_handler(
+    ctx: &serenity::Context,
+    event: &FullEvent,
+    _framework: poise::FrameworkContext<'_, Data, Error>,
+    _data: &Data,
+) -> Result<(), Error> {
+    match event {
+        serenity::FullEvent::Message { new_message } => {
+            if new_message.content.contains("fuck") {
+                let response = format!("Hey {}, don't say bad words.", new_message.author.name);
+                if let Err(e) = new_message.channel_id.say(&ctx.http, response).await {
+                    eprintln!("Error sending message: {:?}", e);
+                };
+                // ctx.say(response).await?;
+            }
+        },
+        _ =>{}, 
+    };
+    Ok(())
+}
 
 #[poise::command(slash_command, prefix_command)]
 async fn age(
@@ -24,7 +45,13 @@ async fn main() {
     let intents = serenity::GatewayIntents::non_privileged();
 
     let framework = poise::Framework::builder()
-        .options(poise::FrameworkOptions { commands: vec![age()], ..Default::default() })
+        .options(poise::FrameworkOptions { 
+            commands: vec![age()],
+            event_handler: |ctx, event, framework, data| {
+                Box::pin(event_handler(ctx, event, framework, data))
+            },
+            ..Default::default() 
+        })
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
